42246718afa3ebfaf37eb288edf2c1d08c2781d9
Denis Putnam <denisputnam@verizon.net>
2025-02-06 16:28:29 -0500

    added bootstrap.

--- expenses/forms.py
+++ expenses/forms.py
@@ -3,7 +3,18 @@ from django import forms
 from .models import Expense
 
 
+# class ExpenseForm(forms.ModelForm):
+#     class Meta:
+#         model = Expense
+#         fields = ['vendor_name', 'due_date', 'amount', 'date_paid']
+
 class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
         fields = ['vendor_name', 'due_date', 'amount', 'date_paid']
+        widgets = {
+            'vendor_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'due_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'amount': forms.NumberInput(attrs={'class': 'form-control'}),
+            'date_paid': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+        }
